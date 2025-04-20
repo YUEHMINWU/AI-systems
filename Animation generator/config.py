3c4c00b2e4ffb5676afc99bd4ef5e7a54b2f70de
YUEHMINWU <135212736+YUEHMINWU@users.noreply.github.com>
2025-04-20 12:48:03 +0800

    Update config.py

--- Animation generator/config.py
+++ Animation generator/config.py
@@ -14,9 +14,9 @@ class Config:
         "outline": 1
     }
     TTS_VOICE = "en-US-AriaNeural"
-    SDXL_ENDPOINT = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-    HF_TOKEN = os.environ['HF_TOKEN']
-    FALAI_KEY = os.environ['FALAI_KEY']
+    SDXL_ENDPOINT = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0" # Choose the text-to-image model in Hugging Face Hub
+    HF_TOKEN = os.environ['HF_TOKEN'] # export HF_TOKEN = "Your_API_Key"
+    FALAI_KEY = os.environ['FALAI_KEY'] # export FALAI_KEY = "Your_API_Key"
 
     @staticmethod
     def get_scene_duration(num_scenes):
